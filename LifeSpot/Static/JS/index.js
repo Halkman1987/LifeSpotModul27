6c4496da0392186b217c712a2e5b01503f2619fe
Halkman <halk-man@mail.ru>
2022-06-26 16:08:37 +0500

    js

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,12 @@
+﻿ <script>
+let aGe = prompt("Enter your age");
+
+        if (aGe >=18)
+{
+        alert("Hello man")
+}
+        else
+{
+        alert("So sorry...you little");
+        window.location.href = "http://www.google.com";
+}
